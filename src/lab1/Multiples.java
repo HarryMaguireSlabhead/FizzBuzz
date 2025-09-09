a39fc65817cc071ca04223e2ac34d6f42f7a2e67
Johnson Qin <johnsonqin0@gmail.com>
2025-09-09 19:55:41 -0400

    Editing Multiples.java

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,10 +2,12 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int count = 0;
         for (int i = 1; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
-                System.out.println(i);
+                count++;
             }
         }
+        System.out.println(count + " integers below 1000 are greater than 0 and are multiples of 3 or 5");
     }
 }
